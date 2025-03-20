175240da8bb285eb693c0ffce7efa87087013283
Rafael <raas@cin.ufpe.br>
2025-03-20 19:11:19 -0300

    conectando com o back

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+    async rewrites() {
+        return [
+          {
+            source: "/api/:path*", // Mapeia chamadas para /api/*
+            destination: "http://127.0.0.1:3000/:path*", // Redireciona para o backend
+          },
+        ];
+      },
+
 };
 module.exports = nextConfig
