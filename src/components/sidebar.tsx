167a64d6d591fe3ce5a845edacb416646073c18d
felipetmacedo <ftm2@cin.ufpe.br>
2025-03-23 15:32:29 -0300

    fix(sidebar): remove modifications on the sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -61,99 +61,77 @@ export function Sidebar({ chats, isCollapsed, isMobile }: SidebarProps) {
           </div>
         </div>
       )}
-      <div className="flex flex-col justify-between h-full ">
-        <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
-          {chats.map((chat, index) =>
-            isCollapsed ? (
-              <TooltipProvider key={index}>
-                <Tooltip key={index} delayDuration={0}>
-                  <TooltipTrigger asChild>
-                    <Link
-                      href="#"
-                      className={cn(
-                        buttonVariants({ variant: chat.variant, size: "icon" }),
-                        "h-11 w-11 md:h-16 md:w-16",
-                        chat.variant === "secondary" &&
+      <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        {chats.map((chat, index) =>
+          isCollapsed ? (
+            <TooltipProvider key={index}>
+              <Tooltip key={index} delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className={cn(
+                      buttonVariants({ variant: chat.variant, size: "icon" }),
+                      "h-11 w-11 md:h-16 md:w-16",
+                      chat.variant === "secondary" &&
                         "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white",
-                      )}
-                    >
-                      <Avatar className="flex justify-center items-center">
-                        <AvatarImage
-                          src={chat.avatar}
-                          alt={chat.avatar}
-                          width={6}
-                          height={6}
-                          className="w-10 h-10 "
-                        />
-                      </Avatar>{" "}
-                      <span className="sr-only">{chat.name}</span>
-                    </Link>
-                  </TooltipTrigger>
-                  <TooltipContent
-                    side="right"
-                    className="flex items-center gap-4"
+                    )}
                   >
-                    {chat.name}
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            ) : (
-              <Link
-                key={index}
-                href="#"
-                className={cn(
-                  buttonVariants({ variant: chat.variant, size: "lg" }),
-                  chat.variant === "secondary" &&
+                    <Avatar className="flex justify-center items-center">
+                      <AvatarImage
+                        src={chat.avatar}
+                        alt={chat.avatar}
+                        width={6}
+                        height={6}
+                        className="w-10 h-10 "
+                      />
+                    </Avatar>{" "}
+                    <span className="sr-only">{chat.name}</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  className="flex items-center gap-4"
+                >
+                  {chat.name}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ) : (
+            <Link
+              key={index}
+              href="#"
+              className={cn(
+                buttonVariants({ variant: chat.variant, size: "lg" }),
+                chat.variant === "secondary" &&
                   "dark:bg-muted  dark:hover:bg-muted dark:hover:text-white shrink",
-                  "justify-start gap-4",
+                "justify-start gap-4",
+              )}
+            >
+              <Avatar className="flex justify-center items-center">
+                <AvatarImage
+                  src={chat.avatar}
+                  alt={chat.avatar}
+                  width={6}
+                  height={6}
+                  className="w-10 h-10 "
+                />
+              </Avatar>
+              <div className="flex flex-col max-w-28">
+                <span>{chat.name}</span>
+                {chat.messages.length > 0 && (
+                  <span className="text-xs truncate ">
+                    {chat.messages[chat.messages.length - 1].name.split(" ")[0]}
+                    :{" "}
+                    {chat.messages[chat.messages.length - 1].isLoading
+                      ? "Typing..."
+                      : chat.messages[chat.messages.length - 1].message}
+                  </span>
                 )}
-              >
-                <Avatar className="flex justify-center items-center">
-                  <AvatarImage
-                    src={chat.avatar}
-                    alt={chat.avatar}
-                    width={6}
-                    height={6}
-                    className="w-10 h-10 "
-                  />
-                </Avatar>
-                <div className="flex flex-col max-w-28">
-                  <span>{chat.name}</span>
-                  {chat.messages.length > 0 && (
-                    <span className="text-xs truncate ">
-                      {chat.messages[chat.messages.length - 1].name.split(" ")[0]}
-                      :{" "}
-                      {chat.messages[chat.messages.length - 1].isLoading
-                        ? "Typing..."
-                        : chat.messages[chat.messages.length - 1].message}
-                    </span>
-                  )}
-                </div>
-              </Link>
-
-            ),
-          )}  
-        </nav>
-        <div
-          className={cn(
-            "flex items-center py-2",
-            isCollapsed ? "justify-center" : "justify-start px-10"
-          )}
-        >
-          <Avatar className="flex justify-center items-center">
-            <AvatarImage
-              src={
-          "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
-              }
-              alt={"teste"}
-              width={6}
-              height={6}
-              className="w-10 h-10 "
-            />
-          </Avatar>
-          {!isCollapsed && <span className="ml-4">User Name</span>}
-        </div>
-      </div>
+              </div>
+            </Link>
+          ),
+        )}
+      </nav>
     </div>
   );
 }
