82a87596faa867bd016c2335f92f6f1bdd0496ca
Mend25 <vma3@cin.ufpe.br>
2025-03-23 14:36:08 -0300

    Implementando página de perfil de usuário

---
+++ src/services/user.ts
@@ -0,0 +1,29 @@
+import api from "./api";
+
+interface UserProps {
+    photo: string;
+    name: string;
+}
+
+const getUserInfo = async () => {
+  try {
+    
+    const response = await api.get(`/users/`);
+    const { photo, name }: UserProps = response.data;
+    return { photo, name };
+  } catch (error) {
+    throw error;
+  }
+}
+
+const editUserInfo = async (data: UserProps) => {
+  try {
+    const response = await api.patch("/users/", data); 
+    return response.data;
+  } catch (error) {
+    console.error('Error editing user info:', error);
+    throw error;
+  }
+}
+
+export { getUserInfo, editUserInfo };
